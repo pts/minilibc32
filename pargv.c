f60ebf7cc44c2a0dbbe58b0fb25f763cb8ad41d7
Peter Szabo <pts@fazekas.hu>
2022-12-01 18:47:57 +0100

    added more invocations; added printing the output of GetCommandLineA()

--- pargv.c
+++ pargv.c
@@ -6,6 +6,21 @@
  * Compile for Linux i386: gcc -D__MINILIBC32__ -Wl,-z,norelro -Wl,--build-id=none -s -static -m32 -mregparm=3 -fno-pic -fno-stack-protector -fomit-frame-pointer -fno-ident -ffreestanding -fno-builtin -fno-unwind-tables -fno-asynchronous-unwind-tables -nostdlib -nostdinc -Os -falign-functions=1 -mpreferred-stack-boundary=2 -falign-jumps=1 -falign-loops=1 -march=i386 -ansi -pedantic -W -Wall -Werror=implicit-function-declaration -o pargv pargv.c minilibc32.o && sstrip pargv
  * Compile for Win32: owcc -bwin32 -Wl,runtime -Wl,console=3.10 -Os -s -fno-stack-check -march=i386 -W -Wall -Wextra -o pargv.exe pargv.c
  * Compile for Win32: i686-w64-mingw32-gcc -m32 -mconsole -s -Os -W -Wall -Wextra -o pargvm.exe pargv.c
+ *
+ * Run:
+ *
+ *   $ ./runpargv.pl ./pargv >linux.out
+ *   $ ./runpargv.pl ./pargv - >linux_dash.out
+ *   # The wine runs below took about 100 seconds each.
+ *   $ ./runpargv.pl wine pargv.exe    >wine_1.6.2_openwatcom.out
+ *   $ ./runpargv.pl wine pargv.exe  - >wine_1.6.2_openwatcom_dash.out
+ *   $ ./runpargv.pl wine pargvm.exe   >wine_1.6.2_mingw.out
+ *   $ ./runpargv.pl wine pargvm.exe - >wine_1.6.2_mingw_dash.out
+ *   # diff linux.out wine_1.6.2_mingw.out  # No differences.
+ *   # diff linux.out wine_1.6.2_openwatcom.out  # Many differences.
+ *   $ ./runpargv.pl dosbox.kvd --cmd --mem-mb=2 mwperun.exe pargv.exe   >dosbox_mwperun.out
+ *   # Many bugs because of how DOSBox concatenates argv to a command-line string.
+ *   $ ./runpargv.pl dosbox.kvd --cmd --mem-mb=2 mwperun.exe pargv.exe - >dosbox_mwperun_dash.out
  */
 
 #if defined(__WATCOMC__) && defined(_WIN32) && defined(_M_I386)
@@ -30,6 +45,7 @@
    __declspec(dllimport) BOOL   __stdcall ReadFile(HANDLE hFile, LPVOID lpBuffer, DWORD nNumberOfBytesToRead, LPDWORD lpNumberOfBytesRead, LPOVERLAPPED lpOverlapped);
    __declspec(dllimport) LPSTR  __stdcall GetCommandLineA(void);
    __declspec(dllimport) LPWSTR __stdcall GetCommandLineW(void);
+   #define WINAPI_DECLARED
 #  define NULL ((void *)0)
 #  define STD_INPUT_HANDLE    ((DWORD)-10)
 #  define STD_OUTPUT_HANDLE   ((DWORD)-11)
@@ -63,6 +79,10 @@
    static const int STDOUTFD = 1;
 #endif
 
+#if defined(_WIN32) && !defined(WINAPI_DECLARED)  /* GetCommandLineA(...). */
+#  include <processenv.h>  /* GetCommandLineA(...) in MinGW. <windows.h> works everywhere, <winbase.h> doesn't. */
+#endif
+
 static int my_strlen(const char *s) {
   const char *s0 = s;
   for (; *s != '\0'; ++s) {}
@@ -75,6 +95,16 @@ int main(int argc, char **argv) {
 #if defined(O_BINARY) && O_BINARY  /* Win32, msvcrt.dll (usually MinGW) */
   setmode(fd, O_BINARY);
 #endif
+  if (argv[1][0] == '-') {
+#ifdef _WIN32
+    const char *gca = GetCommandLineA();
+    (void)!write(fd, "? ", 2);
+    (void)!write(fd, gca, my_strlen(gca));
+    (void)!write(fd, "\n", 1);
+#else
+    (void)!write(fd, "??\n", 3);
+#endif
+  }
   for (++argv; *argv; ++argv) {
     (void)!write(fd, *argv, my_strlen(*argv));
     (void)!write(fd, "\n", 1);
